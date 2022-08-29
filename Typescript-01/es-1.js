a9ff9f0dbccb9fa1c3342421db871e1538775728
Tiziano Forte <forte.tiziano@gmail.com>
2022-08-29 15:41:43 +0200

    1 exercises

---
+++ Typescript-01/es-1.js
@@ -0,0 +1,12 @@
+"use strict";
+export const __esModule = true;
+// Add type annotations to each of these variables.
+var country = "Italy";
+var year = undefined;
+var averageTemperature = 23.6;
+var visited = true;
+var currency = null;
+// Change the type annotations on these variables so they are correct.
+var population = 47450795;
+var isSummer = false;
+var languages = "Spanish, English";
