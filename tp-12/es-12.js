6c5539e3fad75e37f7d0233b35cbc4b674adf08c
Tiziano Forte <forte.tiziano@gmail.com>
2022-08-30 16:10:37 +0200

    12 exercise

---
+++ tp-12/es-12.js
@@ -0,0 +1,22 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+exports.__esModule = true;
+// Prevent the array from being modified.
+// Use the generic interface type `ReadonlyArray`: ReadonlyArray<type>
+// This will cause a type error. Remove the code that now has a type error.
+var languages = [
+    "Mongolian",
+    "French",
+    "Basque",
+    "Thai",
+];
+/* languages[2] = "Hausa"; */
+console.log(languages);
+// Prevent the array from being modified.
+// Use the readonly modifier and the array literal type syntax: readonly type[]
+// This will cause a type error. Remove the code that now has a type error.
+var currencies = ["NZD", "THB", "NGN", "EUR", "COP", "INR"];
+/* currencies.reverse(); */
+console.log(currencies);
