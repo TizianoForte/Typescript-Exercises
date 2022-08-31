0a3330ccfddfe64af1c7578aa5b6b9590e28115f
Tiziano Forte <forte.tiziano@gmail.com>
2022-08-31 11:30:47 +0200

    21 exercise

---
+++ tp-21/es-21.js
@@ -0,0 +1,35 @@
+"use strict";
+/**
+ * START: Follow the instructions below.
+ */
+exports.__esModule = true;
+var countryA = {
+    code: "CN",
+    population: 1412600000
+};
+var countryB = {
+    name: "Greece",
+    code: "GR",
+    languages: ["Greek"],
+    currency: "€",
+    population: 10678632
+};
+var countryC = {
+    name: "Italy",
+    code: "IT",
+    languages: ["Italian"],
+    population: 60317116
+};
+/* countryC.population = 60_317_117; */
+console.log(countryC);
+var countryD = {
+    name: "New Zealand",
+    code: "NZ",
+    population: 5135300
+};
+var countryE = {
+    name: "Thailand",
+    code: "TH",
+    languages: ["Thai", "Isan", "Kam Mueang", "Pak Tai", "Malay"],
+    currency: "Baht"
+};
