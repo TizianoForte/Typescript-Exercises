7f3c11bdc36ece18f471360f4a8a65477a2a8239
Tiziano Forte <forte.tiziano@gmail.com>
2022-08-31 11:19:52 +0200

    13 exercise

---
+++ tp-13/es-13.js
@@ -0,0 +1,13 @@
+/**
+ * START: Follow the instructions below.
+ */
+// Add a tuple type annotation for this tuple.
+var countryPopulation = ["China", 1412600000];
+var countryCurrency = ["Italy", "Euro", "EUR"];
+// Add a tuple type annotation that prevents this tuple from being modified.
+// Use the readonly modifier and the tuple type syntax: readonly [type, type]
+// This will cause a type error. Remove the code that now has a type error.
+var countryLanguage = ["Greece", "Greek"];
+/* countryLanguage.reverse(); */
+console.log(countryLanguage);
+// ----
